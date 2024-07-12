c7e013d96e3407f62cb334482484d5cb8e378856
Asfand Yar <asfandyar687@gmail.com>
2024-07-13 00:55:08 +0500

    feat: added sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,62 @@
+import {
+  CalendarIcon,
+  EnvelopeClosedIcon,
+  FaceIcon,
+  GearIcon,
+  PersonIcon,
+  RocketIcon,
+} from "@radix-ui/react-icons"
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
+import { CreditCard, Folder, LayoutDashboard, Settings, User } from "lucide-react"
+import Link from "next/link"
+
+const Sidebar = () => {
+  return (
+    <Command className="bg-secondary rounded-none">
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>
+            <LayoutDashboard className="mr-2 h-4 w-4" />
+            <Link href="/dashboard">Dashboard</Link>
+          </CommandItem>
+          <CommandItem>
+            <Folder className="mr-2 h-4 w-4" />
+            <Link href="#">Categories</Link>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard className="mr-2 h-4 w-4" />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  )
+}
+
+export default Sidebar
